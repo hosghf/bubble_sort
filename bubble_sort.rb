5f439cb80c562ebeb30bf058f2fab842b768930c
husein <hosghf@gmail.com>
2017-01-25 11:49:42 +0330

    this is first sort programm

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(a)
+  l=a.length-1
+    for j in (0..l)
+    for i in (0..l-1)
+      if(a[i]>a[i+1])
+      	tmp=a[i]
+      	a[i]=a[i+1]
+      	a[i+1]=tmp
+      end
+    end
+    l-=1
+  end
+  a
+end
+
+x=[5,3,6,1,2]
+puts bubble_sort(x).inspect
